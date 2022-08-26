7c506358771fab7986acb1123e60a4e6e3eba66e
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-08-26 18:06:09 +0800

    write the passing test

--- DvdLibraryBestPrac/src/classes/Library.java
+++ DvdLibraryBestPrac/src/classes/Library.java
@@ -5,13 +5,16 @@ import java.util.Collection;
 
 public class Library {
 
+	private ArrayList<Movie> catalogue = new ArrayList<>();
+
 	public Collection<Movie> getCatalogue() {
 		// TODO Auto-generated method stub
-		return new ArrayList<>();
+		return catalogue;
 	}
 
 	public void donate(Movie movie) {
 		// TODO Auto-generated method stub
+		catalogue.add(movie);
 		
 	}
 
