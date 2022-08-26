052c8cedb9841e0350dada74483222a55699aabc
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-08-26 17:51:57 +0800

    Assert first then create the classes. Reference always comes first.

---
+++ DvdLibraryBestPrac/src/classes/Library.java
@@ -0,0 +1,17 @@
+package classes;
+
+import java.util.Collection;
+
+public class Library {
+
+	public Collection<Movie> getCatalogue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void donate(Movie movie) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- DvdLibraryBestPrac/src/test/DonateMovieTest.java
+++ DvdLibraryBestPrac/src/test/DonateMovieTest.java
@@ -1,12 +1,22 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+
+import classes.Library;
+import classes.Movie;
 
 public class DonateMovieTest {
 	
 	@Test
 	public void donateMovie() {
+		//Start by asking the question that we want to ask
+		Library library = new Library();
+		Movie movie = new Movie();
+		library.donate(movie);
 		
+		assertTrue(library.getCatalogue().contains(movie));
 	}
 
 }
